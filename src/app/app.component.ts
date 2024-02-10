a08972f2ed5a83a60d53e60840807953d8ec3ce6
Christophe Mathieu <r09016598@student.thomasmore.be>
2024-02-10 17:54:59 +0100

    Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./utils/header/header.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { CvComponent } from "./pages/cv/cv.component";
+import { ExperienceComponent } from "./pages/experience/experience.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { FooterComponent } from "./utils/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [
+      RouterOutlet,
+      HeaderComponent,
+      HomeComponent,
+      CvComponent,
+      ExperienceComponent, 
+      ContactComponent, 
+      FooterComponent
+    ]
 })
-export class AppComponent {
-  title = 'Portfoliowebsite';
-}
+export class AppComponent { }
